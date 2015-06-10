e7d657a78f394b3ece4e9d25022c322070563571
matthew <matthewwolak@gmail.com>
2015-06-10 10:40:00 +0100

    update makeAinv2.R & ainv.cc
      - add 2 methods/algorithms to calculate Ainv
      - notation in makeAinv2.R describes differences
      - still only using modified version of Meuwissen & Luo algorithm

--- R/makeAinv2.R
+++ R/makeAinv2.R
@@ -3,31 +3,37 @@
 # written by Jarrod Hadfield
 #in the 'MCMCglmm' package
 ################################################
-pedigree <- Mrode2; algorithm <- "ML"; det <- FALSE
-#makeAinv2 <- function(pedigree, algorithm = c("H", "ML"), det = FALSE){
-  numped <- numPed(pedigree)
-  N <- dim(numped)[1]
-  dnmiss <- which(numped[, 2] != -998)
-  snmiss <- which(numped[, 3] != -998)
-  bnmiss <- which(numped[, 2] != -998 & numped[, 3] != -998)
-  Tinv.row <- c(numped[, 1][dnmiss], numped[, 1][snmiss], 1:N)
-  Tinv.col <- c(numped[, 2][dnmiss], numped[, 3][snmiss], 1:N)
+#rm(list = ls()); pedigree <- Mrode2; algorithm <- "MLb"; det <- FALSE
+makeAinv2 <- function(pedigree, algorithm = c("H", "MLa", "MLb"), det = FALSE){
+  al2use <- match.arg(algorithm, choices = c("H", "MLa", "MLb"))
+  if(al2use == "MLb"){
+     renPed <- order(genAssign(pedigree), pedigree[, 2], pedigree[, 3], na.last = FALSE)
+     nPed <- numPed(pedigree[renPed, ])
+  } else nPed <- numPed(pedigree)
+  N <- dim(nPed)[1]
+  dnmiss <- which(nPed[, 2] != -998)
+  snmiss <- which(nPed[, 3] != -998)
+  Tinv.row <- c(nPed[, 1][dnmiss], nPed[, 1][snmiss], 1:N)
+  Tinv.col <- c(nPed[, 2][dnmiss], nPed[, 3][snmiss], 1:N)
   Tinv.x <- c(rep(-0.5, length(dnmiss) + length(snmiss)), rep(1, N))
   el.order <- order(Tinv.col + Tinv.row/(N + 1), decreasing = FALSE)
-  Tinv <- Matrix(0, N, N, sparse = TRUE)
-  Tinv[1, 2] <- 1
-  Tinv@i <- as.integer(Tinv.row[el.order] - 1)
-  Tinv@p <- as.integer(c(match(1:N, Tinv.col[el.order]), length(el.order) + 1) - 1)
-  Tinv@x <- as.double(Tinv.x[el.order])
-  nA <- N + length(dnmiss) + length(snmiss)
-  nA <- nA + sum(duplicated(paste(numped[, 2], numped[, 3])[bnmiss]) == FALSE)
   inbreeding <- c(rep(0, N), -1)
-  numped[numped == -998] <- N + 1
-  al2use <- match.arg(algorithm, choices = c("H", "ML"))
+  dii <- rep(0, N)
+
+
   if(al2use == "H"){
-    Cout <- .C("acinv",
-	    as.integer(numped[, 2] - 1), #dam
-	    as.integer(numped[, 3] - 1),  #sire
+     bnmiss <- which(nPed[, 2] != -998 & nPed[, 3] != -998)
+     Tinv <- Matrix(0, N, N, sparse = TRUE)
+     Tinv[1, 2] <- 1
+     Tinv@i <- as.integer(Tinv.row[el.order] - 1)
+     Tinv@p <- as.integer(c(match(1:N, Tinv.col[el.order]), length(el.order) + 1) - 1)
+     Tinv@x <- as.double(Tinv.x[el.order])
+     nA <- N + length(dnmiss) + length(snmiss)
+     nA <- nA + sum(duplicated(paste(nPed[, 2], nPed[, 3])[bnmiss]) == FALSE)
+     nPed[nPed == -998] <- N + 1
+     Cout <- .C("acinv",
+	    as.integer(nPed[, 2] - 1), #dam
+	    as.integer(nPed[, 3] - 1),  #sire
 	    as.double(inbreeding),  #f
             as.integer(Tinv@i),  #iTinvP
 	    as.integer(c(Tinv@p, length(Tinv@x))),  #pTinvP
@@ -49,43 +55,89 @@ pedigree <- Mrode2; algorithm <- "ML"; det <- FALSE
 
 
 
-   if(al2use == "ML"){
-      Ainv.row <- c(seq(N), dnmiss, snmiss, ) 
-#     dii <- rep(0, N)
-     Cout <- .C("acinvML",
-	    as.integer(numped[, 2] - 1), #dam
-	    as.integer(numped[, 3] - 1),  #sire
+   if(al2use == "MLa" | al2use == "MLb"){
+      sTinv <- sparseMatrix(i = as.integer(Tinv.row[el.order] - 1),
+	p = as.integer(c(match(1:N, Tinv.col[el.order]), length(el.order) + 1) - 1),
+	x = TRUE, index1 = FALSE, dims = c(N, N), symmetric = FALSE,
+	dimnames = list(as.character(nPed[, 1]), NULL))
+      nPed[nPed == -998] <- N + 1
+      # transpose gives lower triangle
+      Ainv <- t(crossprod(crossprod(Diagonal(n = N, x = TRUE), sTinv)))
+      #system.time(Ainv2 <- t(as(crossprod(as(crossprod(Diagonal(n = N, x = TRUE), sTinv), "lgCMatrix")), "lsCMatrix")))
+#identical(Ainv@i, Ainv2@i)
+#identical(Ainv@p, Ainv2@p)
+   }
+if(al2use == "MLa"){
+# differs from H algorithm:
+  # 1: doesn't use matrix multiplication (Tinv %*% Dinv %*% tTinv) to create Ainv, instead assigns element-by-element the non-zero Ainv entries (will be useful when adding genetic groups to the algorithm)
+     Cout <- .C("acinvMLa",
+	    as.integer(nPed[, 2] - 1), #dam
+	    as.integer(nPed[, 3] - 1),  #sire
 	    as.double(inbreeding),  #f
             as.double(rep(0, N)),  #dii
-#            as.integer(Tinv@i),  #iTinvP
-#	    as.integer(c(Tinv@p, length(Tinv@x))),  #pTinvP
-#            as.double(Tinv@x),  #xTinvP
             as.integer(N),   #n
-#	    as.integer(length(Tinv@x)), #nzmaxTinvP
+            as.double(rep(0, length(Ainv@x))),  # xA
+	    as.integer(Ainv@i), #iA
+	    as.integer(Ainv@p), #pA
+	    as.integer(length(Ainv@x))) #nzmaxA
+     Ainv@x <- Cout[[6]]
+     Ainv <- as(Ainv, "dgCMatrix")
+}
 
-            as.double(rep(0, 6*N)))  # xA
-	    as.integer(rep(0, nA)), #iA
-	    as.integer(rep(0, N + 1)), #pA
-	    as.integer(nA)) #nzmaxA
-# Triplets are lower triangle!
-#order of triplets in xA: i,i; i,sire; i,dam; sire,sire; sire,dam/dam,sire; dam,dam;  
-#     Ainv <- Matrix(0, N, N, dimnames = list(as.character(pedigree[, 1]), NULL))
-#     Ainv[1, 2] <- 1
-#     Ainv@i <- Cout[[9]][1:Cout[[12]]]
-#     Ainv@p <- Cout[[10]]
-#     Ainv@x <- Cout[[11]][1:Cout[[12]]]
+if(al2use == "MLb"){
+# differs from MLa: 
+  # 1: Adds Ainv elements in same for loop as calculation of f
+  # 2: First checks to see if individual k has same dam and sire as k-1, if so then just assigns k-1's f 
+  # 3: simplifies the calculation of the addition to the Ainv element (instead of alphai * 0.25 - defines alphai=alphai*0.25).
 
-tmp <- cbind(i = c(rep(seq(N), 3), numped[, 3], apply(numped[, 2:3], MARGIN = 1, FUN = max), numped[, 2]),
-	j = c(seq(N), numped[, 3], numped[, 2], numped[, 3], apply(numped[, 2:3], MARGIN = 1, FUN = min), numped[, 2]),
-	x = Cout[[6]])[tmp[, 1] != N+1 & tmp[, 2] != N+1, ]
+#warning("Not producing correct Ainv as of 2pm on 8 June 2015")
+cat("10:24am\n")
+     Cout <- .C("acinvMLb",
+	    as.integer(nPed[, 2] - 1), #dam
+	    as.integer(nPed[, 3] - 1),  #sire
+	    as.double(inbreeding),  #f
+            as.double(rep(0, N)),  #dii
+            as.integer(N),   #n
+            as.double(rep(0, length(Ainv@x))),  # xA
+	    as.integer(Ainv@i), #iA
+	    as.integer(Ainv@p), #pA
+	    as.integer(length(Ainv@x))) #nzmaxA
+     Ainv@x <- Cout[[6]]
+     fsOrd <- as(as.integer(renPed), "pMatrix")
+     Ainv <- as(t(fsOrd) %*% Ainv %*% fsOrd, "dgCMatrix")
 
-      Ainv <- sparseMatrix(i = tmp[, 1], j = tmp[, 2], x = tmp[, 3])
-#     Ainv <- as(Ainv, "dgCMatrix")
-   }
 
+}
 
    if(det) logDet <- -1*determinant(Ainv, logarithm = TRUE)$modulus[1] else logDet <- NULL
-
- return(list(Ainv = Ainv, listAinv = sm2list(Ainv, rownames = pedigree[, 1], colnames = c("row", "column", "Ainv")), f = Cout[[3]][-(N+1)], logDet = logDet))
+ return(list(Ainv = Ainv, listAinv = sm2list(Ainv, rownames = pedigree[, 1], colnames = c("row", "column", "Ainv")), f = Cout[[3]][-(N+1)], dii = dii, logDet = logDet))
 }
+
+
+#library(devtools); dev_mode(TRUE); install.packages("~/Dropbox/nadiv_parent/nadiv_2.13.4.tar.gz", repos = NULL); library(nadiv); library(asreml)
+
+#ped <- Mrode2
+#ped <- warcolak[, 1:3]
+#ped <- read.table("~/Dropbox/GarlandMice/pedigree_fixed_switched.txt", header = TRUE)
+#ped <- read.table("~/Dropbox/DataMenagerie/Liedvogel2012_TitPeds/BlueTit.txt", header = TRUE)
+#ped <- read.table("~/Dropbox/DataMenagerie/Liedvogel2012_TitPeds/GreatTit.txt", header = TRUE)
+
+#ped <- data.frame(id = seq(10), dam = c(NA, NA, 2, 2, 4, 5, 2, 2, 5, NA), sire = c(NA, NA, 1, 1, 1, 3, 3, 3, 6, 7)) 
+
+#system.time(Ainv <- makeAinv(ped)$Ainv)
+#system.time(AinvH <- nadiv:::makeAinv2(ped, "H")$Ainv)
+#system.time(AinvMLa <- nadiv:::makeAinv2(ped, "MLa")$Ainv)
+#system.time(AinvMLb <- nadiv:::makeAinv2(ped, "MLb")$Ainv)
+#system.time(AinvA <- asreml.Ainverse(ped)$ginv)
+
+
+#sum(Ainv - AinvH); range(Ainv - AinvH)
+
+#sum(Ainv - AinvMLa); range(Ainv - AinvMLa)
+
+#sum(Ainv - AinvMLb); range(Ainv - AinvMLb)
+
+#sum(AinvMLa - AinvMLb); range(AinvMLa - AinvMLb)
+
+
 
--- src/ainv.cc
+++ src/ainv.cc
@@ -143,29 +143,22 @@ void acinv(
 //   M&L algorithm below
 extern "C"{  
 
-void acinvML(
+void acinvMLa(
         int *dam,       
         int *sire,         
         double *f, 
         double *dii,    
-//	int *iTinvP,              
-//	int *pTinvP,	         
-//	double *xTinvP,
         int *n,
-//	int *nzmaxTinvP,
-
-        double *xA
+        double *xA,
 	int *iA,
 	int *pA,
 	int *nzmaxA
 ){         
 
-  int     j, k, h, cnt, sj, dj, tstart;
+  int     j, k, h, cnt, sj, dj, istart;
   double  ai, alphai;
   double  *AN = new double[2*n[0]];
   double  *li = new double[n[0]];
-//  cs *Tinv, *D, *tTinv, *tTD;
-//  cs *Ainv;
 
   for(k=0; k<n[0]; k++){
      li[k]=0.0;               // set l to zero
@@ -217,9 +210,6 @@ void acinvML(
       }
     }  // end of while
     f[k] = ai-1.0;
-// ADDED bit
-//FIXME: try sticking her the part with the separate for loop through k<n[0] (N individs)
-// END added bit
     for(h=0; h<n[0]; h++){
       li[h]  = 0.0;            // reset l to zero except l_ii =1
     }
@@ -227,42 +217,205 @@ void acinvML(
 
 
 
-//  Ainv = cs_spalloc(n[0], n[0], nzmaxA[0], true, false);
-  cnt = 0;
   for(k=0; k<n[0]; k++){  // iterate through each individual 
-
-// LEFT OFF HERE
-      alphai = sqrt(1.0/(dii[k]));
-      tstart = k*6;
+      alphai = 1.0/(dii[k]);
+      istart = pA[k];
       // i,i
-      xA[tstart] += alphai;
-      if(sire[j] != n[0]){
-         // i,sire
-         xA[tstart+1] += alphai * -0.5;
+      xA[istart] += alphai;
+      sj = sire[k];
+      dj = dam[k];
+      if(sj != n[0]){
+         istart = pA[sj];
          // sire,sire
-         xA[tstart+3] += alphai * 0.25;
-      }
-      if(dam[j] != n[0]){
-         // i,dam
-         xA[tstart+2] += alphai * -0.5;
-         // dam,dam
-         xA[tstart+5] += alphai * 0.25;
-         if(sire[j] != n[0]){
-            //sire,dam/dam,sire
-            xA[tstart+4] += alphai * 0.25;
+         xA[istart] += alphai * 0.25;
+         // sire,dam
+         if(sj <= dj){
+            if(dj != n[0]){
+               for(j=istart; j<pA[sj+1]; j++){
+                 if(iA[j] == dj){
+                   xA[j] += alphai * 0.25;
+                   break;
+                 }
+               }
+            }
          }
+         // sire,i
+         for(j=istart; j<pA[sj+1]; j++){
+            if(iA[j] == k){
+              xA[j] += alphai * -0.5;
+              break;
+            }
+         }
+      }
+      if(dj != n[0]){
+         istart = pA[dj];
+         // dam,dam
+         xA[istart] += alphai * 0.25;
+         for(j=istart; j<pA[dj+1]; j++){
+         // sire
+         // dam,i
+            if(iA[j] == k){
+              xA[j] += alphai * -0.5;
+            }
+         }
+         // dam,sire
+         if(dj <= sj){
+            if(sj != n[0]){
+               for(j=istart; j<pA[dj+1]; j++){
+                 if(iA[j] == sj){
+                   xA[j] += alphai * 0.25;
+                   break;
+                 }
+               }
+            }
+         }
+
       }
   }
 
-  
-//  for (k = 0 ; k < tTD->nzmax; k++){
-//    iAP[k] = tTD->i[k];
-//    xAP[k] = tTD->x[k];
-//  }
-//  for (k = 0 ; k <= tTD->n; k++){
-//    pAP[k] = tTD->p[k];
-//  }
-//  nzmaxAP[0] = tTD->nzmax;
+  delete[] AN;
+  delete[] li;
+}
+}
+
+
+
+
+
+/////////////////////////
+//   M&L algorithm below
+extern "C"{  
+
+void acinvMLb(
+        int *dam,       
+        int *sire,         
+        double *f, 
+        double *dii,    
+        int *n,
+        double *xA,
+	int *iA,
+	int *pA,
+	int *nzmaxA
+){         
+
+  int     j, k, h, cnt, sj, dj, istart;
+  double  ai, alphai;
+  double  *AN = new double[2*n[0]];
+  double  *li = new double[n[0]];
+
+  for(k=0; k<n[0]; k++){
+     li[k]=0.0;               // set l to zero
+  }
+  for(k=0; k<n[0]; k++){
+     AN[k]=-1;               // set AN to zero
+  }
+    
+  for(k=0; k<n[0]; k++){  // iterate through each row of l 
+    dii[k] = 0.5-0.25*(f[dam[k]]+f[sire[k]]);
+
+    if((k > 0) && (dam[k] == dam[k-1]) && (sire[k] == sire[k-1])){
+      f[k] += f[k-1];
+    } 
+    else {
+      li[k] = 1.0;                   // set l_ii to one
+      ai=0.0;                        // set a_ii to zero
+      j=k;
+      cnt=0;
+
+      while(j>=0){
+
+        sj=sire[j];
+        dj=dam[j];
+
+        if(sj!= n[0]){
+          AN[cnt] = sj;
+          li[sj] += 0.5*li[j];
+          cnt++;
+        }
+
+        if(dj!= n[0]){ 
+          AN[cnt] = dj;
+          li[dj] += 0.5*li[j];
+          cnt++;
+        }
+
+        ai += li[j]*li[j]*dii[j];
+
+        j=-1;
+
+        for(h=0; h<cnt; h++){   // find eldest individual
+          if(AN[h]>j){
+            j = AN[h];
+          }
+        }
+        for(h=0; h<cnt; h++){   // delete duplicates
+          if(AN[h]==j){
+            AN[h] -= n[0]; 
+          }
+        }
+      }  // end of while
+      f[k] = ai-1.0;
+      for(h=0; h<=k; h++){
+        li[h]  = 0.0;            // reset l to zero except l_ii =1
+      }
+
+    } // end else for checking if k has same parents as k-1
+// ADDED bit
+      alphai = 1.0/(dii[k] * 4.0);
+      istart = pA[k];
+      // i,i
+      xA[istart] += alphai * 4.0;
+      sj = sire[k];
+      dj = dam[k];
+      if(sj != n[0]){
+         istart = pA[sj];
+         // sire,sire
+         xA[istart] += alphai;
+         // sire,dam
+         if(sj <= dj){
+            if(dj != n[0]){
+               for(j=istart; j<pA[sj+1]; j++){
+                 if(iA[j] == dj){
+                   xA[j] += alphai; 
+                   break;
+                 }
+               }
+            }
+         }
+         // sire,i
+         for(j=istart; j<pA[sj+1]; j++){
+            if(iA[j] == k){
+              xA[j] += alphai * -2.0;
+              break;
+            }
+         }
+      }
+      if(dj != n[0]){
+         istart = pA[dj];
+         // dam,dam
+         xA[istart] += alphai;
+         for(j=istart; j<pA[dj+1]; j++){
+         // sire
+         // dam,i
+            if(iA[j] == k){
+              xA[j] += alphai * -2.0;
+            }
+         }
+         // dam,sire
+         if(dj <= sj){
+            if(sj != n[0]){
+               for(j=istart; j<pA[dj+1]; j++){
+                 if(iA[j] == sj){
+                   xA[j] += alphai;
+                   break;
+                 }
+               }
+            }
+         }
+
+      }
+// END added bit
+  } // end of for
 
   delete[] AN;
   delete[] li;
