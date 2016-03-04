8a0108520dd431757fa89eecfe76d78a6b5ee4bb
Matthew <matthewwolak@gmail.com>
2016-03-04 14:21:07 +0000

    enable `LRTest()` with positive log-likelihoods
      - Assume that lower positive log-likelihood is better fit (minimize positive log-likelihood)
      - CHECK this assumption with data

--- R/LRTest.R
+++ R/LRTest.R
@@ -2,13 +2,16 @@
 # Tests the hypothesis that the reduced model offers a better fit
 # Helpful reading: section 6.4.1.1 of Bolker 2008. pp. 189-194
 #NOTE: sometimes ASReml-R returns positive log-likelihoods, other times negative log-likelihoods. My experience so far is that positive values are returned when there are often boundary parameters or the model is having trouble fitting the data. 
-#	 ***!!!ASSUMPTION that could be implemented!!!*** 
-#	when both are positive, the likelihood is being maximized (i.e., the greater value is a greater likelihood.
+#	 ***!!!ASSUMPTION:*** 
+##  	when both are positive, the likelihood is being maximized (i.e., the greater value is a greater likelihood.
 LRTest <- function(full, reduced, df = 1, boundaryCorrection = FALSE){
     if(sign(full) != sign(reduced)){
        stop("Signs of the log-likelihoods are opposite - or 1 log-likelihood is zero...don't know what to do")
     }
-    if(sign(full) > 0) stop("positive log-likelihoods")
+    # positive log-likelihoods: better fit has lower log-likelihood (less positive)
+    if(sign(full) > 0){
+      lambda <- 2*((-1*full) - (-1*reduced))
+    
     # negative log-likelihoods: better fit has greater log-likelihood (less negative)
     if(sign(full) < 0){
        lambda <- 2*(full - reduced)
