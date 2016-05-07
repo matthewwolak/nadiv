f07d7ce1934b4ce9eb7658a8f4b10c0b5834d732
matthew <matthewwolak@gmail.com>
2016-05-07 23:05:16 +0100

    add methods for prunePed.R
     - profiling shows a lot of time/memory wasted dealing with non-numPed attributes of a numPed class pedigree
     - make a default and numPed method
     - export methods
     - update prunePed.Rd

--- R/prunePed.R
+++ R/prunePed.R
@@ -1,9 +1,17 @@
+# Generic
+prunePed <- function(pedigree, phenotyped, ...){
+  UseMethod("prunePed", pedigree)
+}
+
+###############################################################################
+# Methods:
+
 ################################################
 #Borrowed heavily from the 'prunePed' function
 # written by Jarrod Hadfield
 #in the 'MCMCglmm' package
 ################################################
-prunePed <- function (pedigree, phenotyped) {
+prunePed.default <- function (pedigree, phenotyped, ...) {
    nPed <- numPed(pedigree[, 1:3])
    ikeep <- keep <- match(phenotyped, pedigree[, 1])
    nind <- length(ikeep) + 1
@@ -18,5 +26,19 @@ prunePed <- function (pedigree, phenotyped) {
    pedigree[, 2] <- as.factor(as.character(pedigree[, 2]))
    pedigree[, 3] <- as.factor(as.character(pedigree[, 3]))
  pedigree
+}
+
+
+######################################
+
+prunePed.numPed <- function (pedigree, phenotyped, ...) {
+   ikeep <- keep <- match(phenotyped, pedigree[, 1])
+   nind <- length(ikeep) + 1
+   while(length(ikeep) != nind){
+      nind <- length(ikeep)
+      ikeep <- union(c(pedigree[ikeep, 2:3]), ikeep)
+      ikeep <- ikeep[which(ikeep > 0)]
+   }
+ pedigree[sort(ikeep), ]
 }
 
