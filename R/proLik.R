3288c4873f4a6c85a93a209888d445467edc4566
matthew <matthewwolak@gmail.com>
2017-06-12 16:58:17 +0100

    create loop to seek out proLik CIs more thoroughly
      - optimize now re-run if final LRT not close enough to alpha value LRT
      - tolerance based on how far off alpha will accept

--- R/proLik.R
+++ R/proLik.R
@@ -1,7 +1,7 @@
 proLik <- function(full.model, component,
 	G = TRUE, negative = FALSE,
 	nsample.units = 3, nse = 3,
-	alpha = 0.05, tolerance = 1e-6,
+	alpha = 0.05, tolerance = 0.001,
 	parallel = FALSE, ncores = getOption("mc.cores", 2L)){
   s2 <- full.model$sigma2
   #TODO check if `component` is in  `paste0("V", seq(length(full.model$gammas)))`
@@ -19,6 +19,7 @@ proLik <- function(full.model, component,
   if(is.na(std.err) | std.err == 0) std.err <- 0.1 
   full.mod2 <- asreml::update.asreml(object = full.model, start.values = TRUE)$gammas.table
   chi.val <- 0.5 * qchisq(alpha, df = 1, lower.tail = FALSE)
+  chi.tol <- chi.val - 0.5 * qchisq(alpha + tolerance, df = 1, lower.tail = FALSE)
 
   proLik_keep_uniQUe_UCL <- list(gam = NULL, lambdas = NULL)
   tmpLRTU <- function(st, chi){
@@ -34,41 +35,44 @@ proLik <- function(full.model, component,
       proLik_keep_uniQUe_LCL[[2]] <<- c(proLik_keep_uniQUe_LCL[[2]], lrt)
       abs(chi - lrt)
   }
-
-
-  Uint <- c(gamma.est, gamma.est + (nse * std.err))
-  Lint <- c(gamma.est - (nse*std.err), gamma.est)
-  if(!negative & Lint[1] < 0) Lint[1] <- 1e-8
-  #FIXME next two lines assume correlations and won't work for covariance
-  if(negative == TRUE & Uint[2] > 1) Uint[2] <- 1.0 - 1e-8
-  if(negative == TRUE & Lint[1] < -1) Lint[1] <- -1.0 + 1e-8
-  if(parallel){
-     tmpUCL <- parallel::mcparallel(expr = expression(c(optimize(f=tmpLRTU, interval = Uint, chi = chi.val, tol = tolerance), proLik_keep_uniQUe_UCL)))
-
-  } else{
-       UCL <- optimize(f = tmpLRTU, interval = Uint, chi = chi.val, tol = tolerance)
+  UCL <- LCL <- list(minimum = NA, objective = chi.val)
+  cnt <- 0  #TODO determine if maximum of 3 is enough
+  while(((UCL[[2L]] > chi.tol) + (LCL[[2L]] > chi.tol)) > 0 & cnt < 3){
+    if(cnt == 0){
+      Uint <- c(gamma.est, gamma.est + (nse * std.err))
+      Lint <- c(gamma.est - (nse * std.err), gamma.est)
+    } else{
+        if(UCL[[2L]] > chi.tol){
+          Uext <- chi.val / max(proLik_keep_uniQUe_UCL$lambdas)
+          Uint <- c(gamma.est + (nse * std.err), gamma.est + (Uext*(nse*std.err) + std.err))
+        }
+        if(LCL[[2L]] > chi.tol){
+          Lext <- chi.val / max(proLik_keep_uniQUe_LCL$lambdas)
+          Lint <- c(gamma.est - (Lext*(nse*std.err) + std.err), gamma.est - (nse*std.err))
+        }          
+      }
+    if(!negative & Lint[1L] < 0) Lint[1L] <- 1e-8
+      if(!negative & Lint[2L] < 0) Lint[2L] <- 1e-7
+    #FIXME next two lines assume correlations and won't work for covariance
+    if(negative == TRUE & Uint[2L] > 1) Uint[2L] <- 1.0 - 1e-8
+    if(negative == TRUE & Lint[1L] < -1) Lint[1L] <- -1.0 + 1e-8
+    if(parallel & UCL[[2L]] > chi.tol){
+       tmpUCL <- parallel::mcparallel(expr = expression(c(optimize(f=tmpLRTU, interval = Uint, chi = chi.val), proLik_keep_uniQUe_UCL)))
+    } else{
+        if(UCL[[2L]] > chi.tol){
+          UCL <- optimize(f = tmpLRTU, interval = Uint, chi = chi.val)
+        }
+      }
+    if(LCL[[2L]] > chi.tol){
+      LCL <- optimize(f = tmpLRTL, interval = Lint, chi = chi.val)
     }
-  LCL <- optimize(f = tmpLRTL, interval = Lint, chi = chi.val, tol = tolerance)
-  if(parallel){
-     tmpUCL.out <- parallel::mccollect(tmpUCL, wait = TRUE)[[1]]
-     UCL <- list(minimum = tmpUCL.out$minimum[[1]], objective = tmpUCL.out$objective[[1]])
-     proLik_keep_uniQUe_UCL <- list(gam = tmpUCL.out$gam, lambdas = tmpUCL.out$lambdas)
+    if(parallel & UCL[[2L]] > chi.tol){
+      tmpUCL.out <- parallel::mccollect(tmpUCL, wait = TRUE)[[1]]
+      UCL <- list(minimum = tmpUCL.out$minimum[[1]], objective = tmpUCL.out$objective[[1]])
+      proLik_keep_uniQUe_UCL <- list(gam = tmpUCL.out$gam, lambdas = tmpUCL.out$lambdas)
+    }
+    cnt <- cnt + 1
   }
-
-  # Check if CI limits have in fact been found
-  #TODO can below be set in `while` and replace above code so no repetition?
-  ## Tolerance set so LRT stat no further away than 1/2 of an alpha percentile
-  chi.tol <- chi.val - 0.5 * qchisq(alpha + 0.005, df = 1, lower.tail = FALSE)
-  if(all((chi.val - proLik_keep_uniQUe_LCL$lambdas) > chi.tol)){
-    Lext <- chi.val / max(proLik_keep_uniQUe_LCL$lambdas)
-    Lint <- c(gamma.est - (Lext*(nse*std.err) + std.err), gamma.est - (nse*std.err))
-    if(!negative & Lint[1] < 0) Lint[1] <- 1e-8
-    #FIXME next line assumes correlations and won't work for covariance
-    #if(negative == TRUE & Lint[1] < -1) Lint[1] <- -1.0 + 1e-8
-    #TODO does this replace previous LCL: these evaluations should be kept
-    LCL <- optimize(f = tmpLRTL, interval = Lint, chi = chi.val, tol = tolerance)
-  }
-  #TODO same as above for UCL
 
   gamma.vec <- c(gamma.est, seq(gamma.est - (gamma.est - LCL$minimum)/2, gamma.est + (UCL$minimum - gamma.est)/2, length.out = nsample.units))
 
@@ -87,8 +91,12 @@ proLik <- function(full.model, component,
       warning("Boundary parameter: confidence interval estimation may produce strange behavior - proceed with caution)")
       }
 
+  # Remove CI limit estimates if they haven't been reached
+  if(UCL[[2L]] > chi.tol) UCL[[1L]] <- NA
+  if(LCL[[2L]] > chi.tol) LCL[[1L]] <- NA
 
-return(structure(list(lambdas = profile$lambdas[ord.index], 
+
+ return(structure(list(lambdas = profile$lambdas[ord.index], 
 	var.estimates = profile$var.estimates[ord.index] * s2, 
 	UCL = UCL$minimum * s2, 
 	LCL = LCL$minimum * s2, 
