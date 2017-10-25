ffcd1fe9acd38aaa72ee7ce068ea05327c03e261
matthew <matthewwolak@gmail.com>
2017-10-25 13:46:03 -0500

    tidy and add approximations to Sd to makeSdsim

--- R/makeSdsim.R
+++ R/makeSdsim.R
@@ -15,6 +15,11 @@ makeSdsim <- function(pedigree, heterogametic, N,
     return(NULL)
   }
 
+  approxSd <- makeSd(pedigree, heterogametic = heterogametic, DosageComp = DosageComp,
+	parallel = parallel, ncores = ncores,
+	invertSd = invertSd, returnS = returnS)
+  lapproxSd <- summary(approxSd$Sd)
+
   n <- dim(pedigree)[1L]
   nPed <- numPed(pedigree[, 1:3])
   damsex <- pedigree[unique(nPed[, 2])[-1], 4]
@@ -23,8 +28,7 @@ makeSdsim <- function(pedigree, heterogametic, N,
     pedigree <- pedigree[, c(1,3,2,4)]
     names(pedigree) <- pedname
     nPed <- numPed(pedigree[, 1:3])
-   cat("Assuming female heterogametic (e.g., ZZ/ZW) sex chromosome system\n")
-  } else cat("Assuming male heterogametic (e.g., XX/XY) sex chromosome system\n")
+  }
   sex <- rep(-998, n)
   sex[homs <- which(pedigree[,4] != heterogametic)] <- 1
   sex[hets <- which(pedigree[,4] == heterogametic)] <- 0
@@ -69,28 +73,29 @@ makeSdsim <- function(pedigree, heterogametic, N,
   Sdsim@i <- Cout[[8]][1:nSd]
   Sdsim@p <- Cout[[9]][1:(nhom+1)]
   Sdsim@x <- Cout[[10]][1:nSd] / N
-#TODO in `makeDsim()` stick in the diagonal the values from the analytical D?
-## If do, don't need to calculate the diagonals in c++ code ('sdsim.cc')
+  diag(Sdsim) <- diag(approxSd$Sd)
+  #TODO don't *have* to calculate diagonals in c++ ('sdsim.cc') because of above line
+  ## But make sure doesn't mess up below combining of `lapproxSd` and `summary(Sdsim)`
 
-#FIXME decide what to do (and below where return)
-#  approxSd <- makeSd(pedigree, homogametic = homogametic, DosageComp = DosageComp,
-#	parallel = parallel, ncores = ncores,
-#	invertSd = invertSd, returnS = returnS, det = det)
-
-  lapproxSd <- summary(Sdsim)
-  names(lapproxSd)[3L] <- "simSd"
+  lapproxSd$simSd <- summary(Sdsim)$x
   listSdsim <- NULL
   if(calcSE) {
-#TODO check that this is the same for sex-chromosomal case as it is for autosomes
-## Could see how it would differ, because difference chances of inheriting certain alleles
-## Not strictly binomial sampling
-     lapproxSd$Sdse <- vapply(lapproxSd$simSd, FUN = function(x, N){(sqrt(x * (1 - x))) / sqrt(N)}, FUN.VALUE = vector("numeric", 1), N)
+#TODO check that SE calc is the same for sex-chromosomal case as it is for autosomes
+## Could differ, because different chances of inheriting certain alleles
+## Not strictly binomial sampling in same way as for autosomes
+     lapproxSd$simSdse <- vapply(lapproxSd$simSd, FUN = function(x, N){(sqrt(x * (1 - x))) / sqrt(N)}, FUN.VALUE = vector("numeric", 1), N)
      listSdsim <- lapproxSd
   } 
 
   cat(".done", "\n")
   logDetSdsim <- determinant(Sdsim, logarithm = TRUE)$modulus[1]
-  
+
+
+
+#XXX LEFT OFF HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11  
+
+
+
   if(invertSd){
     cat("inverting Sdsim ...")
     Sdsiminv <- solve(Sdsim)
@@ -111,21 +116,5 @@ makeSdsim <- function(pedigree, heterogametic, N,
 
 
 }
-
-
-
-
-################################################################################
-
-#pedFS <- simPedHS(1, 1, 4)
-#makeSdsim(pedFS, heterogametic = "M", N = 10000, returnS = TRUE)
-#SdOut <- makeSd(pedigree, heterogametic = "M", returnS = TRUE)
-
-#pedDFC <- simPedDFC(1)
-#Sd <- makeSdsim(pedDFC, heterogametic = "M", N = 1000000, returnS = TRUE)$Sdsim
-
-#makeSdsim(FG90, heterogametic = "0", N = 100000, returnS = TRUE)$Sdsim
-
-
 
 
