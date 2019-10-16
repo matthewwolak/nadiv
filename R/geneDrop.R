94f0393234ec9b77920856e509fbde267325dc4a
matthewwolak <matthewwolak@gmail.com>
2019-10-15 21:22:29 -0500

    add methods for geneDrop, particularly numPed class pedigrees

--- R/geneDrop.R
+++ R/geneDrop.R
@@ -1,3 +1,5 @@
+# Generic
+
 #' Functions to conduct gene dropping through a pedigree
 #' 
 #' Functions that perform and summarize gene dropping conducted on supplied pedigrees
@@ -15,7 +17,7 @@
 #' individual in the pedigree has two unknown parents it will have the following
 #' two allelic values: 1=maternal haplotype and -1=paternal haplotype.  
 #'
-#' @aliases geneDrop
+#' @aliases geneDrop geneDrop.default geneDrop.numPed
 #' @param pedigree A pedigree with columns organized: ID, Dam, Sire.
 #' @param N The number of times to iteratively trace alleles through the
 #'   pedigree
@@ -42,10 +44,18 @@
 #'   geneDrop(Mrode2, N = 10
 #' 
 #' @export
+geneDrop <- function(pedigree, N, parallel = FALSE, ncores = getOption("mc.cores", 2L), ...){
+  UseMethod("geneDrop", pedigree)
+}
 
+###############################################################################
+# Methods:
+#' @rdname geneDrop
+#' @method geneDrop default
+#' @export
 geneDrop <- function(pedigree, N, parallel = FALSE, ncores = getOption("mc.cores", 2L), ...){
 
-  nPed <- if(is.numPed(pedigree)) pedigree else numPed(pedigree)
+  nPed <- numPed(pedigree)
   n <- nrow(pedigree)
   dfounders <- which(nPed[, 2] == -998)
   sfounders <- which(nPed[, 3] == -998)
@@ -71,5 +81,39 @@ geneDrop <- function(pedigree, N, parallel = FALSE, ncores = getOption("mc.cores
 	maternal = matrix(Cout[[1]], ncol = N, byrow = TRUE),
 	paternal = matrix(Cout[[2]], ncol = N, byrow = TRUE),
 	numericPedigree = nPed))
+}
+
+######################################
+
+#' @rdname geneDrop
+#' @method geneDrop numPed
+#' @export
+geneDrop <- function(pedigree, N, parallel = FALSE, ncores = getOption("mc.cores", 2L), ...){
+
+  n <- nrow(pedigree)
+  dfounders <- which(pedigree[, 2] == -998)
+  sfounders <- which(pedigree[, 3] == -998)
+  #FIXME allow alleles to be specified, 
+  ## but associate user supplied alleles with integers (work on integers in c++)
+  dalleles <- salleles <- vector("integer", length = n) 
+  #TODO allow supplied inbreeding coefficients so founders can be inbred
+  dalleles[dfounders] <- as.integer(dfounders)
+  salleles[sfounders] <- as.integer(-sfounders)
+  Ndalleles <- rep(dalleles, each = N)
+  Nsalleles <- rep(salleles, each = N)
+
+  #TODO execute in parallel
+  Cout <- .C("genedrop",
+	as.integer(Ndalleles),
+	as.integer(Nsalleles),
+	as.integer(N),
+	as.integer(n),
+	as.integer(pedigree[, 2] - 1),
+	as.integer(pedigree[, 3] - 1))
+
+ return(list(IDs = pedigree[, 1],
+	maternal = matrix(Cout[[1]], ncol = N, byrow = TRUE),
+	paternal = matrix(Cout[[2]], ncol = N, byrow = TRUE),
+	numericPedigree = pedigree))
 }
 
